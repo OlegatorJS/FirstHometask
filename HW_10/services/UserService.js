ec6d98d7b43c96c80e27399e161a04e98b29816d
Mr.Parfeniuk <parfenyuk.oleh@gmail.com>
2024-04-09 18:15:13 +0300

    HW_10

---
+++ HW_10/services/UserService.js
@@ -0,0 +1,33 @@
+import UserRepository from '../repositories/UserRepository.js';
+
+export default class UserService {
+    constructor() {
+        this.repository = new UserRepository();
+    }
+
+    async create(name, password) {
+        await this.repository.create({
+            name,
+            password,
+        });
+    }
+
+    async getByNameAndPassword(name, password) {
+        const [user] = await this.repository.getByField('name', name);
+
+        if (user && user.password === password) {
+            return user;
+        }
+
+        return null;
+    }
+
+    async getById(value) {
+        const [user] = await this.repository.getByField('id', value);
+        return user;
+    }
+
+    getAll() {
+        return this.repository.getAll();
+    }
+}
